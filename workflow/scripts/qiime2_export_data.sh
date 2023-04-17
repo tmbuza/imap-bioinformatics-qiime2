9f8a75f043abe28fee4bb4b7a074c839433e6e20
tmbuza <ndelly@gmail.com>
2023-04-17 04:16:54 -0500

    Update qiime2_export_data.sh

--- workflow/scripts/qiime2_export_data.sh
+++ workflow/scripts/qiime2_export_data.sh
@@ -1,29 +1,39 @@
+INPUTDIR="qiime2_process"
+EXPORTDIR="qiime2_process/export"
+
+mkdir -p "${EXPORTDIR}"
+
 # Creating tab-separated feature table
 qiime tools export \
-  --input-path $PWD/qiime2_process/feature-table.qza \
-  --output-path $PWD/qiime2_process/transformed
+  --input-path "${INPUTDIR}"/feature-table.qza \
+  --output-path "${EXPORTDIR}"
 
 # Converting biom to tsv feature table
 biom convert \
-  -i $PWD/qiime2_process/transformed/feature-table.biom \
-  -o $PWD/qiime2_process/transformed/feature-table.tsv --to-tsv
+  -i "${EXPORTDIR}"/feature-table.biom \
+  -o "${EXPORTDIR}"/feature-table.tsv --to-tsv
 
 # Creating tab-separated taxonomy table
 qiime tools export \
-  --input-path $PWD/qiime2_process/taxonomy.qza \
-  --output-path $PWD/qiime2_process/transformed
+  --input-path "${INPUTDIR}"/taxonomy.qza \
+  --output-path "${EXPORTDIR}"
 
 # Tabulating fetature table with taxonomy”
 qiime metadata tabulate \
-  --m-input-file $PWD/qiime2_process/transformed/feature-table.tsv \
-  --m-input-file $PWD/qiime2_process/transformed/taxonomy.tsv \
-  --o-visualization $PWD/qiime2_process/transformed/feature-taxonomy-table.qzv
+  --m-input-file "${EXPORTDIR}"/feature-table.tsv \
+  --m-input-file "${EXPORTDIR}"/taxonomy.tsv \
+  --o-visualization "${INPUTDIR}"/feature-taxonomy-table.qzv
   
 # Exporting unrooted and rooted Newick tree
 qiime tools export \
-  --input-path $PWD/qiime2_process/unrooted-tree.qza \
-  --output-path $PWD/qiime2_process/unrooted-tree \
+  --input-path "${INPUTDIR}"/unrooted-tree.qza \
+  --output-path "${EXPORTDIR}"/unrooted-tree \
 
 qiime tools export \
-  --input-path $PWD/qiime2_process/rooted-tree.qza \
-  --output-path $PWD/qiime2_process/rooted-tree \
+  --input-path "${INPUTDIR}"/rooted-tree.qza \
+  --output-path "${EXPORTDIR}"/rooted-tree \
+
+# Exporting representative sequences to a fasta file
+qiime tools export \
+  --input-path "${INPUTDIR}"/rep-seqs.qza \
+  --output-path "${EXPORTDIR}"/qiime2-rep-seqs
